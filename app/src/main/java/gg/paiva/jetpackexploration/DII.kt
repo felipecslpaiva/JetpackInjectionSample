3f16fdd2ed3d442590cb5b6fbc7d8c3bfb9b83e1
Felipe Paiva <felipe@paiva.gg>
2020-07-07 14:27:41 +0400

    Change to let the instance of Session controller to be handled by koin

--- app/src/main/java/gg/paiva/jetpackexploration/DII.kt
+++ app/src/main/java/gg/paiva/jetpackexploration/DII.kt
@@ -1,17 +1,24 @@
 package gg.paiva.jetpackexploration
 
 import android.content.Context
-import android.util.Log
 import androidx.startup.Initializer
-import gg.paiva.jetpackexploration.session.di.SessionComponent
+import gg.paiva.jetpackexploration.session.di.WorkManagerComponent
+import gg.paiva.jetpackexploration.session.di.sessionModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class DII : Initializer<Unit>{
-
     override fun create(context: Context) {
-        Log.d("" ,"")
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(context)
+            modules(listOf(sessionModule))
+        }
     }
 
     //This will be called even before everything soo all the components are ready
     // before it call the create method
-    override fun dependencies(): List<Class<out Initializer<*>>> = listOf(SessionComponent::class.java)
+    override fun dependencies(): List<Class<out Initializer<*>>> = listOf(WorkManagerComponent::class.java)
 }
